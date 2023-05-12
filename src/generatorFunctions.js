7cb396a39a3aecd0915eb4e60def15e77272d940
Umar <work.umar7@hotmail.com>
2023-05-12 02:56:58 +0100

    added the generatorFunctions module which contains functions used to generate elements

--- src/generatorFunctions.js
+++ src/generatorFunctions.js
@@ -1,5 +1,3 @@
-import './style.css';
-
 /**
  * this function takes an array of classes and adds those classes to the element using a forEach loop
  * @param {Array} classes an array of classes that need to be added to the element
@@ -97,45 +95,3 @@ function inputGenerator(attributes) {
     attributes.forEach(attribute => input.setAttribute(attribute[0], attribute[1]));
     return input;
 }
-
-function titleSection() {
-    const titleContainer = divGenerator(['title']);
-    titleContainer.append(imgGenerator('./../src/assets/page-icon.png', 'a circular icon depicting all 4 seasons', ['main-icon']), headingGenerator('h1', ['page-title'], 'Weather', spanGenerator(['title-end'], 'Watch')));
-    return titleContainer;
-}
-
-function headerFormSection() {
-    const form = formGenerator(['search-form']);
-    form.append(inputGenerator([['type', 'search'], ['id', 'search-bar'], ['placeholder', 'Search a location'], ['required', true]]), imgGenerator('./../src/assets/search-icon.svg', 'A magnifying glass', ['search-button']));
-    return form;
-}
-
-function headerButtons() {
-    const buttonsContainer = divGenerator(['change-degrees']);
-    buttonsContainer.append(buttonGenerator(['celsius-btn', 'btn', 'active'], '°C'), buttonGenerator(['fahrenheit-btn', 'btn', 'active'], '°F'));
-    return buttonsContainer;
-}
-
-function header() {
-    const header = document.createElement('header');
-    header.append(titleSection(), headerFormSection(), headerButtons());
-    return header;
-}
-
-const main = document.querySelector('body');
-main.append(header());
-
-{/* <header>
-        <div class="title">
-            <img class="main-icon" src="./../src/assets/page-icon.png" alt="a circular icon depicting all 4 seasons">
-            <h1 class="page-title">Weather<span class="title-end">Watch</span></h1>
-        </div>
-        <form class="search-form">
-            <input type="search" id="search-bar" placeholder="Search a location" required>
-            <img class="search-button" src="./../src/assets/search-icon.svg" alt="A magnifying glass">
-        </form>
-        <div class="change-degrees">
-            <button class="celsius-btn btn active">°C</button>
-            <button class="fahrenheit-btn btn">°F</button>
-        </div>
-    </header> */}
