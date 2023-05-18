46967f5ce11492be68901c6b3aa032bf07a5c207
Umar <work.umar7@hotmail.com>
2023-05-18 03:08:09 +0100

    added the event listeners onto the relevant buttons so now the page is interactive

--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import { header } from './headerSection';
 import { onLoadDefaultWeather, currentDayInformationContainer } from './currentTemperatureSection';
 import { divGenerator } from './generatorFunctions';
 import { carouselButtons } from './carouselButtons';
+import { dailyAndHourlyButtonLogic, dotLogic, measurementButtonsLogic, moveBackButtonLogic, moveForwardButtonLogic } from './mainButtonsLogic';
 
 const dailyHourlyWeather = divGenerator(['daily-hourly-weather-carousel']); //the div that will contain the daily weather, and the hourly
 dailyHourlyWeather.append(carouselButtons());
 
 document.body.append(header(), currentDayInformationContainer, dailyHourlyWeather);
 window.addEventListener('load', onLoadDefaultWeather);
+
+const hourlyWeatherButton = document.querySelector('.hourly-weather-btn'); //this is the button used to display the hourly weather
+const dailyWeatherButton = document.querySelector('.daily-weather-btn'); //this is the button used to display the weather for the current and next 6 days
+const moveForwardButton = document.querySelector('.move-right'); //this button moves the carousel to the next 6 hours
+const moveBackButton = document.querySelector('.move-left'); //this button moves the carousel to the previous 6 hours
+const dotsContainer = document.querySelector('.dots'); //the container that holds each dot
+const changeDegreesContainer = document.querySelector('.change-degrees'); //the container that holds the celsius and fahrenheit buttons
+
+
+
+dailyWeatherButton.addEventListener('click', dailyAndHourlyButtonLogic);
+hourlyWeatherButton.addEventListener('click', dailyAndHourlyButtonLogic);
+moveForwardButton.addEventListener('click', moveForwardButtonLogic);
+moveBackButton.addEventListener('click', moveBackButtonLogic);
+dotsContainer.addEventListener('click', dotLogic);
+changeDegreesContainer.addEventListener('click', measurementButtonsLogic);
